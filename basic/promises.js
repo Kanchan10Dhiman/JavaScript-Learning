ccc8d39035a008b16705bf5079b63fd4ae370669
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-08-07 19:11:36 +0530

    added content

--- basic/promises.js
+++ basic/promises.js
@@ -4,3 +4,24 @@ let promise1= new Promise((resolve,reject)=>{
     //resolve("succesfully resolved");
     reject("some erroe occurred");
 })
+
+// API direct data return nahi karti, Promise return karti hai.Kyunki data turant available nahi hota, wo future mein milega.API ko server se connect karna padta hai.Internet delay, server response ka wait, etc. JavaScript can’t wait — it gives you a Promise that "data lekr aa rha hi hu m".
+
+function getdata1(dataId,getnextdata){
+    return new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+        //console.log("data",dataId);
+        reject ("rejected");
+        if(getnextdata){
+            getnextdata();
+        }
+        
+     },3000)
+    });
+}
+let promise= getdata1(123);
+// let promise= getdata1(123,()=>{
+//     getdata(2); 
+// });
+
+
