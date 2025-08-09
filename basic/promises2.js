636e584f3f3c4854ae1a613e7874beb1f768d9a8
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-08-09 18:55:34 +0530

    added content

--- basic/promises2.js
+++ basic/promises2.js
@@ -16,3 +16,10 @@ promise.then((res)=>{ //res vo msg h jo hmne resolve me dala h.
 promise.catch((err)=>{//err vo msg h jo hmne reject me dala h.
     console.log("error is there ",err);
 })
+/*
+.then() handles a Promise when it is successfully executed and gives access to the resolved value.
+.catch() handles a Promise when it fails and gives access to the error.
+
+We pass a parameter in .then() or .catch() so that we can receive the value given to resolve() or reject() inside the Promise.
+This allows us to access and display the success message or error message returned by the Promise.
+*/
