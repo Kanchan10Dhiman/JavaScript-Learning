6e906c7d201249d9bf522fa963f650fa9bc43caf
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-08-13 22:21:34 +0530

    add

--- basic/promises3.js
+++ basic/promises3.js
@@ -4,16 +4,31 @@ Promise chaining is the process of executing multiple asynchronous operations in
 It helps avoid callback hell by writing cleaner, more readable code, and each .then() returns a new promise, allowing us to keep chaining further steps.
 */
 
-function asyncfunction(){
+
+function asyncfunc1(){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            console.log("some data");
-            resolve("successfully done");
+            console.log("data1");
+            resolve("success1");
         },4000);
     });
 }
+console.log("fetch data1");
+let p1=asyncfunc1();
+p1.then((res)=>{
+    console.log(res);
+});
 
-let p1=asyncfunction();
+function asyncfunc2(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("data2");
+            resolve("success2");
+        },4000);
+    });
+}
+console.log("fetch data2");
+let p2=asyncfunc2();
 p1.then((res)=>{
     console.log(res);
 });
