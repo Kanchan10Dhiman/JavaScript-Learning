07aae4a8888c3d7a229dc57527159faa06127ede
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-08-20 18:35:23 +0530

    aded file

--- basic/promises4.js
+++ basic/promises4.js
@@ -1,2 +1,17 @@
 //ASYNC AWAIT is better than  >> promise chain >> callback hell 
+/*1. Async Function
+-->async function always returns a promise.
+-->async keyword lagane se function always ek Promise return karta hai.
+
+-->Even agar tum return "hello" likho, wo automatically Promise.resolve("hello") ban jayega.
+*/
+
 //ASYNC-AWAIT easy bnata h asynchronus programming ko.
+//await ka matlab hota hai: “ruko jab tak Promise resolve/reject nahi hota
+
+//await ko hum asyn k andr hi use krte h 
+
+
+
+
+
