806419a8c330c1f20e967cb60d530d77c29244a7
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-08-20 14:08:15 +0530

    added content

--- basic/promises4.js
+++ basic/promises4.js
@@ -1,22 +1,2 @@
-//ASYNC AWAIT
-
-function getdata(dataId){
-    return new Promise((resolve,reject)=>{
-     setTimeout(()=>{
-        console.log("data",dataId);
-        resolve("success");
-        
-     },3000)
-    });
-}
-//ab ye niche vala callbacks se thoda sa thik lgta h dekhne me ..pr fr b messy hojayga jada statements add krne k baad
-// let p=getdata(1);
-//getdata.then((res)=>{--->this line error kuki function pe then ni lgta promise object pe lgta h 
-getdata(1).then((res)=>{
-    return getdata(2);//console.log(res);
-}).then((res)=>{
-   return getdata(3);
-}).then ((res)=>{
-    console.log("successful chaining")
-})
-;
+//ASYNC AWAIT is better than  >> promise chain >> callback hell 
+//ASYNC-AWAIT easy bnata h asynchronus programming ko.
