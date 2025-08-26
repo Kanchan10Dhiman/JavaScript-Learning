8bac7b506c65e2e16a0e896b8af1fd0c617d4240
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-08-26 10:24:31 +0530

    added content

---
+++ basic/FetchAPI.js
@@ -0,0 +1,10 @@
+//API->appliaction programming interface
+/*
+API is a bridge between two systems.
+
+Most important for us = Web APIs (REST, GraphQL, SOAP, gRPC).
+
+JavaScript me APIs call karne ke liye Fetch API ya Axios use hota hai.
+*/
+
+
