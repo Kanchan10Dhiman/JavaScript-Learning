7af6e3e5d573682681b2263deaff11b966e9d63a
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-01 10:03:01 +0530

    added content

--- basic/FetchAPI.js
+++ basic/FetchAPI.js
@@ -1,13 +1,17 @@
 //API->appliaction programming interface
 /*
 API is a bridge between two systems.
-
+GET → Read
+POST → Create
+PUT → Update (whole)
+PATCH → Update (part)
+DELETE → Remove
 Most important for us = Web APIs (REST, GraphQL, SOAP, gRPC).
 
 JavaScript me APIs call karne ke liye Fetch API ya Axios use hota hai.
   -->fetch returns a promise*/
-const url="https://alexwohlbruck.github.io/cat-facts/docs/endpoints/facts.html";
-
+const url="https://catfact.ninja/facts";
+const para= document.querySelector(".para");
 // let promise=fetch(url);
 // console.log(promise);
 
@@ -15,9 +19,13 @@ const url="https://alexwohlbruck.github.io/cat-facts/docs/endpoints/facts.html";
 const getdata=async()=>{
   console.log("getting data..")
   let response=await fetch(url);
-  console.log(response.status);
-}
+  console.log(response);//json format
+  let data=await response.json();
+  // console.log(data);
+  para.innerText=data[0].text;
 
+}
+//json() methodse hum apna actual daat nikalte h js object k through
 /*
 AJAX → Asynchronous JS & XML
 JSON → JavaScript Object Notation
