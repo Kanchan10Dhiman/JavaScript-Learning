1272777078fc21f6c9db617b01843a8a5a4624c0
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-01 20:05:27 +0530

    added content

--- basic/FetchAPI.js
+++ basic/FetchAPI.js
@@ -10,25 +10,30 @@ Most important for us = Web APIs (REST, GraphQL, SOAP, gRPC).
 
 JavaScript me APIs call karne ke liye Fetch API ya Axios use hota hai.
   -->fetch returns a promise*/
-const url="https://catfact.ninja/facts";
+const url="https://catfact.ninja/fact";
 const para= document.querySelector(".para");
 // let promise=fetch(url);
 // console.log(promise);
 
 // to get result from the promise
-const getdata=async()=>{
+const getfact=async()=>{
   console.log("getting data..")
   let response=await fetch(url);
-  console.log(response);//json format
-  let data=await response.json();
-  // console.log(data);
-  para.innerText=data[0].text;
-
+  console.log(response.status);//json format
+  // let data=await response.json();
+  // // console.log(data);
+  // para.innerText=data[0].text;
 }
+// }
 //json() methodse hum apna actual daat nikalte h js object k through
 /*
-AJAX → Asynchronous JS & XML
+//FROMAT TYPES
+-->JSON → {"id":1, "name":"Kanchan"}(key-value style)
+-->XML → <name>Kanchan</name> (tag-based style)
+-->AJAX → depends on what server sends (JSON/XML/text).--its a technique not format
+
 JSON → JavaScript Object Notation
 json() method → returns a second promise that resolves with the result of parsing the response body text as JSON.
 (Input is JSON, output is JS object)
+-->json() ek method h ..isko hum 'response.json()' krke use krte h...iska kaam hota h jb hum data ko fetch () krwate h..to server data ko text form yani jese string format me bhejta h...to hum ye method use rkke usko convert kr dete h js object me taki hum use access kr ske. . ya [] lgake
  */
